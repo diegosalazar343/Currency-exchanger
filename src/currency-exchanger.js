a61dc48b31f58422264a929cf9ed1f896c6b81d4
diegosalazar343 <diegosalazar343@gmail.com>
2020-11-20 15:36:37 -0800

    looking for bug

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,3 +1,20 @@
 export class CurrencyObj {
+  constructor(worldCurrency, inputAmount, moneyRate) {
+    this.USD = moneyRate.conversion.USD,
+    this.AUD = moneyRate.conversion.AUD,
+    this.CAD = moneyRate.conversion.CAD,
+    this.CHF = moneyRate.conversion.CHF,
+    this.EUR = moneyRate.conversion.EUR,
+    this.GBP = moneyRate.conversion.GBP,
+    this.JPY = moneyRate.conversion.JPY,
+    this.worldCurrency = worldCurrency;
+    this.inputAmount = inputAmount;
+    this.outputAmount = this.currencyResults(this.worldCurrency, this.inputAmount)
+  }
 
+  currencyResults(worldCurrency, inputAmount) {
+    let value = this[worldCurrency];
+    let results = value*inputAmount;
+    return result;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import CurrencyService from './currency-exchanger-servivce.js';
+import './../css/styles.css';
+import CurrencyObj from './currency-exchanger.js';
+import CurrencyService from './currency-exchanger-service.js';
 
 $(document).ready(function () {
   $("#submit").submit(function(event) {
-    event.preventDefualt();
+    event.preventDefault();
     let exchange;
     const worldCurrency = $("#world-currency").val();
     const inputAmount = $("#input-amount").val();
-    $("#").html("");
-    $("#").val("");
+    $("#results").html("Amount:");
+    $("#input-amount").val("");
     CurrencyService.getService()
-    .then(function(rateService) {
-      exchange = new CurrencyService(worldCurrency, inputAmount, rateService);
-      $("#results").append(`${exchange.results(worldCurrency, inputAmount)} ${worldCurrency}`);
-    });
-  })
+      .then(function(rateService) {
+        exchange = new CurrencyService(worldCurrency, inputAmount, rateService);
+        $("#results").append(`${exchange.results(worldCurrency, inputAmount)} ${worldCurrency}`);
+      });
+  });
 });
