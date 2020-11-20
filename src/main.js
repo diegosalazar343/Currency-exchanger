7e42dfe1f34e8bf41593c1040bacd4c31f1ab806
diegosalazar343 <diegosalazar343@gmail.com>
2020-11-20 13:46:18 -0800

    add a fetch method

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import './css/styles.css';
 import CurrencyService from './currency-exchanger-servivce.js';
 
 $(document).ready(function () {
-  $("#TODO").submit(function(event) {
+  $("#submit").submit(function(event) {
     event.preventDefualt();
+    let exchange;
+    const worldCurrency = $("#world-currency").val();
+    const inputAmount = $("#input-amount").val();
+    $("#").html("");
+    $("#").val("");
     let promise = CurrencyService.currencyExchanger();
     promise.then(function(response) {
       const body = JSON.parse(response);
